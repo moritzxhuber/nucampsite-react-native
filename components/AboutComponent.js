2718ebe2d242e40657db3fa2c77102d64f3f256b
Moritz Huber <moritz.huber11@gmx.de>
2020-07-06 11:04:01 +0200

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return { partners: state.partners };
@@ -40,6 +41,26 @@ class About extends Component {
       );
     };
 
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <Mission />
